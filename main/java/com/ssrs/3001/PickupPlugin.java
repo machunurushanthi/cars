70bcd18c7d0d46aa2780ef2d4618c67aab33bee7
shanthi reddy <shanthi@infosys .com>
2022-01-06 08:02:56 +0530

    code for cars

---
+++ main/java/com/ssrs/3001/PickupPlugin.java
@@ -0,0 +1,19 @@
+package com.ssrs.3001;
+
+import com.ssrs.framework.extend.plugin.AbstractPlugin;
+import com.ssrs.framework.extend.plugin.PluginException;
+
+/**
+ * @author ssrs
+ */
+public class 3001Plugin extends AbstractPlugin {
+    @Override
+    public void start() throws PluginException {
+
+    }
+
+    @Override
+    public void stop() throws PluginException {
+
+    }
+}
